b4c3838858259c86e6f1eab027f67bc37dab9c98
Baldiotti <augustobaldiotti@hotmail.com>
2023-04-17 10:50:48 -0300

    SetUp PlataformaStreaming e Test

---
+++ codigo/src/PlataformaStreaming.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PlataformaStreaming {
+    private String nome;
+    private HashMap<String, Serie> series;
+    private HashMap<String, Cliente> clientes;
+    private Cliente clienteAtual;
+
+
+    public Cliente login(String nomeUsuario, String senha) {
+        return null;
+    }
+
+    public void adicionarSerie(Serie serie) {
+    }
+
+    public void adicionarCliente(Cliente cliente) {
+    }
+
+    public List<Serie> filtrarPorGenero(String genero) {
+        return null;
+    }
+
+    public List<Serie> filtrarPorIdioma(String idioma) {
+        return null;
+    }
+
+    public List<Serie> filtrarPorQtdEpisodios(int quantEpisodios) {
+        return null;
+    }
+
+    public void registrarAudiencia(Serie serie) {
+    }
+}
---
+++ codigo/test/PlataformaStreamingTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PlataformaStreamingTest {
+    
+}
