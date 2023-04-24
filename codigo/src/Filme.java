5aed9e0d26e154cb97e46daffe3bb309e846b293
Caioarauj <104323358+Caioarauj@users.noreply.github.com>
2023-04-24 10:33:22 -0300

    classe filme

---
+++ codigo/src/Filme.java
@@ -0,0 +1,21 @@
+
+
+public class Filme extends Midia{
+
+    private int duracao;
+
+    public Filme(){
+        super();
+    }
+
+    public Filme(int id,String nome, LocalDate dataLancamento, int duracao){
+        super(id,nome, dataLancamento);
+        this.duracao = duracao;
+    }
+
+
+    public int getDuracao() {
+        return this.duracao;
+    }
+
+}
