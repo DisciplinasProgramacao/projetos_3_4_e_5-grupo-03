31b2f5dc95f3a5f38d9ad18573eefce2fc0ca8ed
Caioarauj <104323358+Caioarauj@users.noreply.github.com>
2023-04-24 10:25:59 -0300

    implementação midia

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -2,5 +2,10 @@ public class App {
     
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Midia m = new Midia(1, "la casa de papel",LocalDate.now(),2,1);
+       System.out.println(m.getGenero());
+       System.out.println(m.getIdioma());
+
     }
 }
---
+++ codigo/src/Midia.java
@@ -0,0 +1,88 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Midia {
+
+    private int id;
+    private String nome;
+    private int audiencia = 0;
+    private LocalDate dataLancamento;
+    private String genero;
+    private String idioma;
+    private static final List<String> GENEROS = new ArrayList<>(Arrays.asList(
+        "ação",
+        "comédia",
+        "suspense",
+        "terror",
+        "romance",
+        "drama",
+        "ficção cinetífica"
+    )); 
+    private static final List<String> IDIOMAS = new ArrayList<>(Arrays.asList(
+        "inglês", "português", "espanhol";
+    ));
+
+    public Midia(){
+    }
+
+    public Midia(int id, String nome, LocalDate dataLancamento, int genero, int idioma){
+        this.id = id;
+        this.nome=nome;
+        this.dataDeLancamento = dataLancamento;
+        this.genero = GENEROS.get(genero);
+        this.idioma = IDIOMAS.get(idioma);
+        
+    }
+
+    public void registrarAudiencia(){
+        this.audiencia++;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAudiencia() {
+        return this.audiencia;
+    }
+
+    public void setAudiencia(int audiencia) {
+        this.audiencia = audiencia;
+    }
+
+    public LocalDate getDataLancamento() {
+        return this.dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getIdioma() {
+        return this.idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+
+}
