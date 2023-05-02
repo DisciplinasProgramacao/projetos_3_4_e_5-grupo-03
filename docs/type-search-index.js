2b1ec047fa4f1557fc1d6922f2736f6deb94dfdd
Daniel Leão <68720438+danielrleao@users.noreply.github.com>
2023-05-02 10:26:05 -0300

    #6  Geração do javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Cliente"},{"p":"<Unnamed>","l":"Filme"},{"p":"<Unnamed>","l":"Midia"},{"p":"<Unnamed>","l":"PlataformaStreaming"},{"p":"<Unnamed>","l":"Serie"}];updateSearchResults();
